f287d9ecc513500362147d8dc7ae455387ab56ee
AngelOrdon02 <3006613200101@ingenieria.usac.edu.gt>
2022-03-10 22:27:19 -0800

    grid

--- main.py
+++ main.py
@@ -2,3 +2,25 @@
 Angel Geovanny Ordón Colchaj
 201905741
 '''
+from tkinter import *
+
+window = Tk()
+
+window.title("Bienvenido a mi APP")
+
+for i in range(3):
+    window.columnconfigure(i, weight=1, minsize=75)
+    window.rowconfigure(i, weight=1, minsize=50)
+
+    for j in range(0, 3):
+        frame = Frame(
+            master=window,
+            relief=RAISED,
+            borderwidth=1
+        )
+        frame.grid(row=i, column=j, padx=5, pady=5)
+
+        label = Label(master=frame, text=f"Row {i}\n Column {j}")
+        label.pack(padx=5, pady=5)
+
+window.mainloop()
